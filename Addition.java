b1d40d03bef937206a01e4da27595eb78a1d37b4
Inampudi <sai-suma.inampudi@capgemini.com>
2022-09-29 14:59:27 +0530

    Addition.java file is added two numbers to addition branch

---
+++ Addition.java
@@ -0,0 +1,14 @@
+public class Addition
+{
+	public static void main(String args[])
+	{
+	
+		int a=39,b=20;
+		int res=a+b;
+		System.out.println("A:"+a);
+		System.out.println("B:"+b);
+		System.out.println("Addition:"+res);
+
+	}
+
+}
